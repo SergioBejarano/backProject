4874a37f07c2d44f365e27c8b70b7d8c713ba43b
Haidex3 <haider.rodriguez-m@mail.escuelaing.edu.co>
2024-12-09 15:42:51 -0500

    Arreglo Administrator

--- src/main/java/edu/eci/cvds/user_management/model/Administrator.java
+++ src/main/java/edu/eci/cvds/user_management/model/Administrator.java
@@ -3,21 +3,23 @@ package edu.eci.cvds.user_management.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 
 /**
  * Represents an administrator in the system.
  * This class is an entity mapped to the "administrators" table in the "public" schema
  * and extends the {@link User} class to inherit common user properties.
- *
+ */
+@Setter
+@Getter
 @Entity
 @Table(name = "administrators", schema = "public")
 public class Administrator extends User {
 
-
     @Column(name = "email", nullable = false)
     private String email;
-
     @Column(name = "name", nullable = false)
     private String name;
 
@@ -29,9 +31,9 @@ public class Administrator extends User {
      * @param password The password for the administrator's account.
      * @param email    The email address of the administrator.
      * @param name     The name of the administrator.
-     *
+     */
     public Administrator(String id, String userName, String password, String email, String name) {
-        super(id, userName, password);
+        super(id,userName,password, "Administrator");
         this.email = email;
         this.name = name;
         setRole("administrator");
@@ -40,45 +42,9 @@ public class Administrator extends User {
     /**
      * Default constructor for JPA.
      * This is required for entity instantiation by JPA.
-     *
+     */
     public Administrator() {
         super();
     }
 
-    /**
-     * Retrieves the email address of the administrator.
-     *
-     * @return The email address of the administrator.
-     *
-    public String getEmail() {
-        return email;
-    }
-
-    /**
-     * Sets the email address of the administrator.
-     *
-     * @param email The email address to be assigned to the administrator.
-     *
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    /**
-     * Retrieves the name of the administrator.
-     *
-     * @return The name of the administrator.
-     *
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * Sets the name of the administrator.
-     *
-     * @param name The name to be assigned to the administrator.
-     *
-    public void setName(String name) {
-        this.name = name;
-    }
 }
-*/
